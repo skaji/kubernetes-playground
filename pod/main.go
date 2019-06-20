c04f3e1e04ff11baded8fe18d5f48fbfd5c18449
Shoichi Kaji <skaji@cpan.org>
2019-06-20 12:10:17 +0900

    hoge

---
+++ pod/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+var podName string
+var startTime string
+
+func init() {
+	podName = os.Getenv("HOSTNAME")
+	startTime = time.Now().Format(time.RFC3339)
+}
+
+func dump(r *http.Request) string {
+	host, port, _ := net.SplitHostPort(r.RemoteAddr)
+	m := map[string]string{
+		"method":          r.Method,
+		"path":            r.URL.String(),
+		"user_agent":      r.Header.Get("User-Agent"),
+		"forwarded_for":   r.Header.Get("X-Forwarded-For"),
+		"forwarded_proto": r.Header.Get("X-Forwarded-Proto"),
+		"forwarded_port":  r.Header.Get("X-Forwarded-Port"),
+		"remote_host":     host,
+		"remote_port":     port,
+		"pod_name":        podName,
+		"start_time":      startTime,
+		"current_time":    time.Now().Format(time.RFC3339),
+	}
+	b, _ := json.Marshal(m)
+	return string(b)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		d := dump(r)
+		fmt.Println(d)
+		fmt.Fprintln(w, d)
+	})
+
+	addr := ":8080"
+	fmt.Println("Start server with addr", addr)
+	http.ListenAndServe(addr, nil)
+}
